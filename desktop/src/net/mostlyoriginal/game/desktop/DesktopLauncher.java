e1716d3ab126c1458796b2e25f06c95eab3049c6
EvilEntity <me@piotrjastrzebski.io>
2015-12-12 13:01:53 +0100

    ld resolution

--- desktop/src/net/mostlyoriginal/game/desktop/DesktopLauncher.java
+++ desktop/src/net/mostlyoriginal/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import net.mostlyoriginal.game.GdxArtemisGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 900;
+		config.height = 600;
 		new LwjglApplication(new GdxArtemisGame(), config);
 	}
 }
